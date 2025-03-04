01095c2d8e5f4c84d17bd54895d68c3eb83e004a
Eden-Lamr <emmanuelmomodu3@gmail.com>
2025-03-04 21:33:38 +0100

    added an env to set base url for client and used it in axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000"; // Adjust the base URL if needed
+const BASE_URL = import.meta.env.VITE_SERVER_BASE_URL // Adjust the base URL if needed
 
 const axiosInstance = axios.create({
 	baseURL: BASE_URL,
